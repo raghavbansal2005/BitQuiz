9b3be7666aa1c5c591a2f1495f549173cc988de6
raghavbansal2005 <raghav.12345bansal@gmail.com>
2020-11-24 13:39:44 -0600

    Finished the login page and styling witht he proper forms and i also added flash messaging  for when you sign up

--- v1/index.js
+++ v1/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const Question = require('./models/question')
+const passport = require("passport");
+const flash = require("connect-flash");
+const LocalStrategy = require("passport-local");
+const session = require('express-session');
+const User = require("./models/user");
+const bodyParser = require("body-parser");
 
 
 
@@ -20,12 +26,57 @@ app.set("view engine", "ejs");
 
 
 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const sessionConfig = {
+  secret: "password",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24,
+    maxAge: 1000 * 60 * 60 * 24
+  } 
+};
+app.use(session(sessionConfig));
+app.use(flash())
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) =>{
+  res.locals.success = req.flash("success");
+  next();
+})
+
+
+
 //  await Question.find({}).then(q => res.send(q));
 
 
-app.get("/", async (req, res) => {
+app.get("/register", async (req, res) => {
     res.render("home");
 });
+
+app.get("/quiz", async (req, res) => {
+  res.render("quiz");
+});
+
+app.get("/login", async (req, res) => {
+  res.render("./users/login");
+})
+
+app.post("/register", async(req, res)=> {
+  const {username, email, password} = req.body;
+  const user = new User({email, username});
+  const registeredUser = await User.register(user, password);
+  req.flash('success', 'Successfully signed up for BQuiz!');
+  console.log(registeredUser);
+  req.flash("Welcome to Bquiz!");
+  res.redirect("/login");
+})
 
 
 
