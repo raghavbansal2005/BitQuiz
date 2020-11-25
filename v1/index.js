4a2e543e4d3aea5bd06be05099e5847da83925a8
raghavbansal2005 <raghav.12345bansal@gmail.com>
2020-11-25 10:22:47 -0600

    f

--- v1/index.js
+++ v1/index.js
@@ -27,7 +27,7 @@ app.set("view engine", "ejs");
 
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const sessionConfig = {
