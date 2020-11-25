250763c6cdd5ac29848471e30ac0d8601093bfbf
raghavbansal2005 <raghav.12345bansal@gmail.com>
2020-11-25 10:22:11 -0600

    f

--- v1/index.js
+++ v1/index.js
@@ -28,7 +28,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const sessionConfig = {
   secret: "password",
