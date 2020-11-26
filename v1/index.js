598a1734b9f99519f7d82d706f142ad2b419fcd9
raghavbansal2005 <raghav.12345bansal@gmail.com>
2020-11-25 18:30:46 -0600

    more typos

--- v1/index.js
+++ v1/index.js
@@ -92,6 +92,7 @@ app.get("/login", async (req, res) => {
 
 app.post("/login", passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), async (req, res) => {
   res.redirect('/quiz');
+  console.log("Successfully Logged In!")
 })
 
 
