f6f55d29dfc24005536b7c35b8cf41d6f4ab8a15
raghavbansal2005 <raghav.12345bansal@gmail.com>
2020-11-27 09:35:02 -0600

    move to pc

--- v1/models/user.js
+++ v1/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
-const { model } = require("./question");
+
 
 const UserSchema = new Schema({
     email: {
